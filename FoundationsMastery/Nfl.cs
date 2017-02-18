2c6ac7b2f0b10e32112c75e316b7195b2d06d43e
Thomas Waters <Thomas.waters05@gmail.com>
2017-02-17 19:20:34 -0800

    adding boilerplate for nfl classes - will present virtual + override via these classes

---
+++ FoundationsMastery/Nfl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoundationsMastery
+{
+    public class Nfl
+    {
+       public string Conference { get; set; }
+       public string Mascot { get; set; }
+       public string City { get; set; }
+       public int Rank { get; set; }
+    }
+}
---
+++ FoundationsMastery/TnTitans.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoundationsMastery
+{
+    public class TnTitans : Nfl
+    {
+       public TnTitans ()
+        {
+            this.City = "Nashville";
+            this.Rank = 17;
+            this.Mascot = "T-rack";
+            this.Conference = "AFC South"
+
+        }
+
+
+    }
+}
